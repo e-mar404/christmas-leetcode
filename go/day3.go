b3a505b99faf31b047124fd19b5fc5e49972f195
e-mar404 <egmarinn@gmail.com>
2024-12-08 12:59:15 -0600

    day 3 part 2

--- go/day3.go
+++ go/day3.go
@@ -20,21 +20,38 @@ func day3() {
 	}
 	
 	part1 := 0
+	part2 := 0
+	enabled := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		pattern := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+		pattern := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)`)
 		
 		matches := pattern.FindAllString(line, 1000);
 
 		for _, command := range matches {
+			if command == "don't()" {
+				enabled = false
+				continue
+			}
+
+			if command == "do()" {
+				enabled = true
+				continue
+			}
+
 			numberPattern := regexp.MustCompile(`\d{1,3}`)
 			numbers := numberPattern.FindAllString(command, 2)
 
 			number1, _ := strconv.Atoi(numbers[0])
 			number2, _ := strconv.Atoi(numbers[1])
 			part1 += number1 * number2 
+
+			if enabled {
+				part2 += number1 * number2
+			}
 		}
 	}
 
 	fmt.Printf("Part 1: %v\n", part1)
+	fmt.Printf("Part 2: %v\n", part2)
 }
