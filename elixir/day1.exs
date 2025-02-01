a05e3005f8337426255a17fa5fc96c4a2a1a8017
e-mar404 <egmarinn@gmail.com>
2025-02-01 10:16:45 -0600

    elixir: day 1 part 2
    true functional bro creating a bunch of unnecessary functions

--- elixir/day1.exs
+++ elixir/day1.exs
@@ -13,7 +13,7 @@ defmodule Day1 do
       left_val = String.to_integer(Enum.at(line, 0))
       right_val = String.to_integer(Enum.at(line, 3))
 
-      {[left_val | left], [right_val | right ]}
+      {[left_val | left], [right_val | right]}
     end)
   end
 
@@ -23,19 +23,40 @@ defmodule Day1 do
 
     {left, right}
   end
+
+  def sum_distances(list1, list2) do
+    list1
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {left_val, index}, total ->
+      right_val = Enum.at(list2, index)
+
+      abs(left_val - right_val) + total
+    end)
+  end
+
+  def freq_acc(list, map) do
+    list
+    |> Enum.reduce(0, fn left_value, acc ->
+      acc + left_value * Map.get(map, left_value, 0)
+    end)
+  end
 end
 
-{left, right} = File.stream!("../inputs/day1.input")
-|> Day1.line_list()
-|> Day1.split_cols()
+{left, right} =
+  File.stream!("../inputs/day1.input")
+  |> Day1.line_list()
+  |> Day1.split_cols()
 
 # Part 1
 {desc_left, desc_right} = Day1.sort_cols({left, right})
 
-Enum.with_index(desc_left)
-|> Enum.reduce(0, fn {left_val, index}, total ->
-  right_val = Enum.at(desc_right, index)
+desc_left
+|> Day1.sum_distances(desc_right)
+|> IO.puts()
 
-  abs(left_val - right_val) + total
-end)
-|> IO.puts
+# Part 2
+freq_right = Enum.frequencies(right)
+
+left
+|> Day1.freq_acc(freq_right)
+|> IO.puts()
