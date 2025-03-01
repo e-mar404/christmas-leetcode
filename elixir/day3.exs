537ddc7c298b572bef9d9e8db0e80164f98b41b3
e-mar404 <egmarinn@gmail.com>
2025-03-01 09:46:53 -0600

    refactor to be make part 2 easier

--- elixir/day3.exs
+++ elixir/day3.exs
@@ -1,28 +1,27 @@
 defmodule Day3 do
-  def read_lines(file_contents) do
+  def read_line(file_contents) do
     file_contents
     |> Stream.map(&(String.split(&1, "\n", trim: true)))
   end
-
+  
   def clean_line(line_stream) do
     line_stream
     |> Stream.map(fn [line] -> Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, line) end) 
-  end
-
-  def evaluate_mul(values_stream) do
-    values_stream
-    |> Stream.map(fn values -> Enum.map(values, &(extract_mul_values/1)) end)
     |> Enum.to_list()
     |> List.flatten()
-    |> Enum.sum()
   end
 
-  def extract_mul_values([expression]) do
-    Regex.scan(~r/\d{1,3}/, expression)
-    |> List.flatten()
-    |> Enum.reduce(1, fn expression_value, acc ->
-      String.to_integer(expression_value) * acc 
-    end)
+  def evaluate_mul_list([head | tail], acc) do
+    value = extract_mul_value(head) 
+    evaluate_mul_list(tail, acc + value)
+  end
+
+  def evaluate_mul_list(_, acc), do: acc
+
+  def extract_mul_value(mul_string) do
+    [[left], [right]] = Regex.scan(~r/\d{1,3}/, mul_string)
+    
+    String.to_integer(left) * String.to_integer(right)
   end
 end
 
@@ -30,7 +29,7 @@ _test_file = "../inputs/day3.test"
 input_file = "../inputs/day3.input"
 
 File.stream!(input_file)
-|> Day3.read_lines()
+|> Day3.read_line()
 |> Day3.clean_line()
-|> Day3.evaluate_mul()
+|> Day3.evaluate_mul_list(0)
 |> IO.inspect()
